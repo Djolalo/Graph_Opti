394625320ee3a12cfef582e60010407b08ab892e
Djolalo <Djilali.bahous@Outlook.com>
2023-01-26 19:46:03 +0100

    construction de matrice d'adjacence depuis fichier terminée

--- src/parcours.c
+++ src/parcours.c
@@ -1,5 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
 typedef struct {
     int nbSommets;
-    int nbArretes;
     int **mat;
-}MatIncidence;
+}MatAdj;
+
+int** allouerMatrice(int nbLignes, int nbColonnes){
+    int **mat;
+    mat= malloc(sizeof(int*)*nbLignes);
+    if(mat==NULL){
+        fprintf(stderr, "Erreur");
+    }
+    for(int i =0; i<nbLignes;i++){
+        mat[i]=malloc(sizeof(int)*nbColonnes);
+    }
+    return mat;
+}
+
+MatAdj creerMatriceVide(int taille){
+    MatAdj res;
+    res.mat=allouerMatrice(taille,taille);
+    res.nbSommets=taille;
+    for ( int i = 0;i<taille;i++){
+        for (int j=0; j<taille;j++){
+            res.mat[i][j]=0;
+        }
+    }
+    return res;
+}
+
+void parcoursMatAdj(MatAdj g){
+    for(int i=0; i<g.nbSommets;i++){
+        for(int j=0; j<g.nbSommets;j++)printf("%d", g.mat[i][j]);
+        puts("");
+    }
+}
+
+MatAdj creerMatAdjFichier(FILE* fd){
+int nbSommets, j , i, tmp;
+MatAdj res;
+    fscanf(fd,"\n#Description du graphe");
+    fscanf(fd,"\nnbSom = %d", &nbSommets);
+    res=creerMatriceVide(nbSommets);
+    for(int z=0;z<nbSommets;z++){
+        fscanf(fd,"\nSom%d:",&j);
+        j--;
+        fscanf(fd,"%d", &tmp);
+        res.mat[j][0]=tmp;
+        i=1;
+        while(fscanf(fd,";%d", &tmp)>0){
+            res.mat[j][i]=tmp;
+            i++;
+        }
+    }
+    return res;
+}
+
+int main(void){
+    FILE* fd;
+    if((fd=fopen("adjacence.txt","r"))==NULL)
+        return 1;
+    MatAdj test= creerMatAdjFichier(fd);
+    fclose(fd);
+    parcoursMatAdj(test);
+    return 0;
+}
